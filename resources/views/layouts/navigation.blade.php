26f0054f4674efb58b0e6a8b5d37aa792355ffbe
Idris Haidir <idris.haidir1@gmail.com>
2026-07-03 22:24:56 +0700

    Memperbaiki Navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -74,7 +74,13 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Dasbor') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('transactions.create')" :active="request()->routeIs('transactions.create')">
+                {{ __('Catat Transaksi') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('transactions.index')" :active="request()->routeIs('transactions.index')">
+                {{ __('Riwayat') }}
             </x-responsive-nav-link>
         </div>
 
